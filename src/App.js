892f292886b3761d7d04712860ce9b0182b03a4e
Amy Jo <amyjojjm2001@gmail.com>
2023-08-05 03:44:27 -0700

    init commit

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import { useEffect, useRef } from "react";
+import "./App.css";
+import Globe from "react-globe.gl";
+
+function App() {
+  const globeEl = useRef();
+
+  const N_PATHS = 10;
+  const MAX_POINTS_PER_LINE = 10000;
+  const MAX_STEP_DEG = 1;
+  const MAX_STEP_ALT = 0.015;
+
+  const gData = [...Array(N_PATHS).keys()].map(() => {
+    let lat = (Math.random() - 0.5) * 90;
+    let lng = (Math.random() - 0.5) * 360;
+    let alt = 0;
+
+    return [
+      [lat, lng, alt],
+      ...[...Array(Math.round(Math.random() * MAX_POINTS_PER_LINE)).keys()].map(
+        () => {
+          lat += (Math.random() * 2 - 1) * MAX_STEP_DEG;
+          lng += (Math.random() * 2 - 1) * MAX_STEP_DEG;
+          alt += (Math.random() * 2 - 1) * MAX_STEP_ALT;
+          alt = Math.max(0, alt);
+
+          return [lat, lng, alt];
+        }
+      ),
+    ];
+  });
+
+  useEffect(() => {
+    const globe = globeEl.current;
+
+    // Auto-rotate
+    globe.controls().autoRotate = true;
+    globe.controls().autoRotateSpeed = 0.5;
+  }, []);
+
+  const N = 5;
+  const arcsData = [...Array(N).keys()].map(() => ({
+    startLat: (Math.random() - 0.5) * 180,
+    startLng: (Math.random() - 0.5) * 360,
+    endLat: (Math.random() - 0.5) * 180,
+    endLng: (Math.random() - 0.5) * 360,
+  }));
+
+  return (
+    <div className="App">
+      <Globe
+        ref={globeEl}
+        pathsData={gData}
+        pathColor={() => ["rgba(0,0,255,0.6)", "rgba(255,0,0,0.6)"]}
+        pathDashLength={0.01}
+        pathDashGap={0.004}
+        pathDashAnimateTime={100000}
+        pathPointAlt={0.1}
+        pathTransitionDuration={1000}
+        arcsData={arcsData}
+        arcColor={() => ["red", "blue"]}
+        arcDashLength={() => Math.random()}
+        arcDashGap={() => Math.random()}
+        arcDashAnimateTime={() => Math.random() * 4000 + 2000}
+        arcAltitude={0.6}
+        globeImageUrl="//unpkg.com/three-globe/example/img/earth-dark.jpg"
+      />
+    </div>
+  );
+}
+
+export default App;
