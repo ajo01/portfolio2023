2542fdf53268ee7cbf03b46606c7c7b8de2261a1
Amy Jo <amyjojjm2001@gmail.com>
2023-08-21 02:28:09 -0700

    eslint

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,10 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "./Nav.module.css";
 
-import { useSelector } from "react-redux";
 const Nav = () => {
-  const detachNav = useSelector((state) => state.ui.detachNav);
-
   const navigate = useNavigate();
 
   const NavDiv = ({ id, text, onClick }) => (
