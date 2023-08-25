566b7825d9b7f79a0724a23a4362ef789df1d354
Amy Jo <amyjojjm2001@gmail.com>
2023-08-25 16:27:27 -0700

    eslint

--- src/components/atoms/Tag/index.js
+++ src/components/atoms/Tag/index.js
@@ -4,39 +4,39 @@ import theme from "../../../styles/colors";
 
 const Tag = ({ text }) => {
   const getColor = (text) => {
-    if (text == "JavaScript") {
+    if (text === "JavaScript") {
       return theme.tags.yellow;
-    } else if (text == "TypeScript") {
+    } else if (text === "TypeScript") {
       return theme.tags.blue;
-    } else if (text == "React") {
+    } else if (text === "React") {
       return theme.tags.lightBlue;
-    } else if (text == "ChakraUI") {
+    } else if (text === "ChakraUI") {
       return theme.tags.green;
-    } else if (text == "Styled Components") {
+    } else if (text === "Styled Components") {
       return theme.tags.purple;
-    } else if (text == "Tensorflow") {
+    } else if (text === "Tensorflow") {
       return theme.tags.purple;
-    } else if (text == "Next.js") {
+    } else if (text === "Next.js") {
       return theme.tags.indigo;
-    } else if (text == "MaterialUI") {
+    } else if (text === "MaterialUI") {
       return theme.tags.purple;
-    } else if (text == "PDFNet") {
+    } else if (text === "PDFNet") {
       return theme.tags.blue;
-    } else if (text == "Storybook") {
+    } else if (text === "Storybook") {
       return theme.tags.pink;
-    } else if (text == "npm") {
+    } else if (text === "npm") {
       return theme.tags.red;
-    } else if (text == "Firebase") {
+    } else if (text === "Firebase") {
       return theme.tags.orange;
-    } else if (text == "D3.js") {
+    } else if (text === "D3.js") {
       return theme.tags.orange;
-    } else if (text == "OpenAI") {
+    } else if (text === "OpenAI") {
       return theme.tags.grey;
-    } else if (text == "Sanity") {
+    } else if (text === "Sanity") {
       return theme.tags.peach;
-    } else if (text == "TailwindCSS") {
+    } else if (text === "TailwindCSS") {
       return theme.tags.lightBlue;
-    } else if (text == "FramerMotion") {
+    } else if (text === "FramerMotion") {
       return theme.tags.pink;
     }
     return "white";
