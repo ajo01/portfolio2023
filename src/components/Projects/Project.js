a6d00b94bebc72986ccd438c549c1c9d7841889b
Amy Jo <amyjojjm2001@gmail.com>
2023-08-26 14:21:11 -0700

    fix broken a links - wrong height

--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -1,24 +1,22 @@
 import React from "react";
 import styles from "./Project.module.css";
 import Tag from "../atoms/Tag";
-const Project = ({ src, desc, link, index, name, tags }) => {
+const Project = ({ src, desc, link, name, tags }) => {
   return (
     <div className={styles.project}>
-      <div className={styles.imgInfo}>
-        <div className={styles.imgContainer}>
-          <img className={styles.img} src={src} alt="" />
-        </div>
-        <div className={styles.info}>
-          <div className={styles.name}>{name}</div>
+      <div className={styles.imgContainer}>
+        <img className={styles.img} src={src} alt="" />
+      </div>
+      <div className={styles.info}>
+        <div className={styles.name}>{name}</div>
 
-          <div className={styles.desc}>{desc}</div>
-          <div className={styles.tags}>
-            {tags.map((text, i) => (
-              <Tag key={i} text={text} />
-            ))}
-          </div>
-          <div>{link && <a href={link}>See here</a>}</div>
+        <div className={styles.desc}>{desc}</div>
+        <div className={styles.tags}>
+          {tags.map((text, i) => (
+            <Tag key={i} text={text} />
+          ))}
         </div>
+        {link && <a href={link}>See here</a>}
       </div>
     </div>
   );
