8125ee767a202e64ada4420bf8043a489dada573
Amy Jo <amyjojjm2001@gmail.com>
2023-08-25 21:36:58 -0700

    eslint

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import GlobePage from "./components/Globe/Globe";
 import { useDispatch } from "react-redux";
 import { setDetachNav } from "./redux/uiSlice";
 import { Route, Routes } from "react-router-dom";
-import Aurora from "./components/Aurora/Aurora";
 
 function App() {
   const scrollableDivRef = useRef(null);
